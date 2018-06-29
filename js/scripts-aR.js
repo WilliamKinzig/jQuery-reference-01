ec7e554d1e0771e2463db0d89b22326478402374
WilliamKinzig <wippawill2@gmail.com>
2018-06-28 19:22:21 -0700

    make buttons function. Complete

--- js/scripts-aR.js
+++ js/scripts-aR.js
@@ -1,16 +1,35 @@
 $(document).ready(function() {
 
-    $("button#green").click(function() {
-      $("body").addClass("green-background");
-    });
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+  // ALTERNATIVE METHOD
+  // $("button#green").click(function() {
+  //   $("body").css("background-color", "green");
+  // });
 
-    $("button#yellow").click(function() {
-      $("body").addClass("yellow-background");
-    });
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
 
-    $("button#red").click(function() {
-      $("body").addClass("red-background");
-    });
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+    // $("button#green").click(function() {
+    //   $("body").addClass("green-background");
+    // });
+    //
+    // $("button#yellow").click(function() {
+    //   $("body").addClass("yellow-background");
+    // });
+    //
+    // $("button#red").click(function() {
+    //   $("body").addClass("red-background");
+    // });
 
 
   // $("p").click(function() {
