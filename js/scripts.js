4dfafc72bc65447ea300017cb8c45c861ed4cb67
WilliamKinzig <wippawill2@gmail.com>
2018-06-28 18:16:57 -0700

    walrus picter show but no hide yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,5 @@
 $(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
-
   $("p").click(function() {
-    alert("This is a paragraph.");
+    $("img").show();
   });
-
-  $("img").click(function() {
-    alert("This is an image.");
-  });
-
-  jQuery("h1").click(function() {
-    alert("This is a header.");
-    alert("I told you, THIS IS A HEADER!");
-  });
-
 });
