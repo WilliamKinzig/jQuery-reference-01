93cfdfcc9be610410a0d2b43c79fd1d02e5465b3
WilliamKinzig <wippawill2@gmail.com>
2018-06-28 19:12:22 -0700

    make buttons function. Minimal

--- js/scripts-aR.js
+++ js/scripts-aR.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+    $("button#green").click(function() {
+      $("body").addClass("green-background");
+    });
+
+    $("button#yellow").click(function() {
+      $("body").addClass("yellow-background");
+    });
+
+    $("button#red").click(function() {
+      $("body").addClass("red-background");
+    });
 
 
   // $("p").click(function() {
